042e263499c9a8047341c5e693de4b62bd72bfbd
vitor.dev <146399948+its-vitor@users.noreply.github.com>
2023-12-20 03:41:30 -0300

    Add files via upload

--- javascript/login.js
+++ javascript/login.js
@@ -1,13 +1,17 @@
 function register() {
-    document.getElementById('register').style.display = 'block';
-    document.getElementById('login').style.display = 'none';
-    document.getElementById('registerButton').classList.add("active")
-    document.getElementById('loginButton').classList.remove("active")
+  document.getElementById("register").style.display = "block";
+  document.getElementById("register-banner").style.display = "flex";
+  document.getElementById("login").style.display = "none";
+  document.getElementById("login-banner").style.display = "none";
+  document.getElementById("registerButton").classList.add("active");
+  document.getElementById("loginButton").classList.remove("active");
 }
 
 function login() {
-    document.getElementById('login').style.display = 'block';
-    document.getElementById('register').style.display = 'none';
-    document.getElementById('loginButton').classList.add("active")
-    document.getElementById('registerButton').classList.remove("active")
+  document.getElementById("login-banner").style.display = "flex";
+  document.getElementById("login").style.display = "block";
+  document.getElementById("register").style.display = "none";
+  document.getElementById("register-banner").style.display = "none";
+  document.getElementById("loginButton").classList.add("active");
+  document.getElementById("registerButton").classList.remove("active");
 }
